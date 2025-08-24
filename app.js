90a06795384962371f69edf6b6f28d2a6af8edef
Sameer-kumar99 <sameer2003karisath@gmail.com>
2025-08-24 17:37:23 +0530

    Elite stay updated

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ import authRoutes from './routes/auth.js';
 import propertyRoutes from './routes/properties.js';
 import bookingRoutes from './routes/bookings.js';
 import './config/passport.js';
+import { fileURLToPath } from 'url';
+import expressLayouts from 'express-ejs-layouts';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -50,6 +53,8 @@ app.use('/', indexRoutes);
 app.use('/', authRoutes);
 app.use('/properties', propertyRoutes);
 app.use('/bookings', bookingRoutes);
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
